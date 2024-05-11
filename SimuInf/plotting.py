d9c444b1e872501bc1756bc2373335d4972b155b
Jiyue Qin <jiyueqincumc@gmail.com>
2024-05-11 16:24:50 -0700

    update plotting for more cuts and directions

--- SimuInf/plotting.py
+++ SimuInf/plotting.py
@@ -1,11 +1,27 @@
+def return_cuts(confset_ls, display_mode, cut, background):
+    if display_mode == 'z':
+        # get the brain image into the right direction
+        confset_ls = [[np.flipud(np.transpose(data[:, :, cut])) for data in list(confset) + [get_data(background)]] for
+                      confset in confset_ls]
+    if display_mode == 'x':
+        confset_ls = [[np.flipud(np.transpose(data[cut, :, :])) for data in list(confset) + [get_data(background)]] for
+                      confset in confset_ls] 
+    if display_mode == 'y':
+        confset_ls = [[np.flipud(np.transpose(data[:, cut, :])) for data in list(confset) + [get_data(background)]] for
+                      confset in confset_ls]        
+    return confset_ls
+
+
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from nilearn.image import get_data
 
 def confset_plot(confset_ls, name_ls, nrow=1, ncol=None, fontsize=20,
-                 figsize=(30, 20), ticks=True, background=None,
-                 cut=None, label_cut=False, title=None):
+                 figsize=(30, 20), ticks=True, background=None, display_mode='z', 
+                 cuts=None, label_cut=False, title=None):
     """
     plot a list of confidence sets
 
@@ -24,8 +40,10 @@ def confset_plot(confset_ls, name_ls, nrow=1, ncol=None, fontsize=20,
       figure size
     background: nifti image, optional, default: None
       the background image
-    cut:int, optional, default: None
-      the z coordinate to slice for a 3d image
+    display_mode: string, optional, default: 'z'
+      the direction to slice the 3d image, options are 'x', 'y', 'z'
+    cuts:list or an integer, optional, default: None
+      a list of coordinates to slice for a 3d image, or an integer if there is only one cut 
     label_cut: bool, optional, default: False
       if True, the title will include the z coordinate for the slice
 
@@ -34,20 +52,33 @@ def confset_plot(confset_ls, name_ls, nrow=1, ncol=None, fontsize=20,
     confset_plot(confset_ls, name_ls)
 
     """
-
     n = len(confset_ls)
+    #print(n)
+    if cuts is not None: 
+        if isinstance(cuts, int):
+            cuts = [cuts]
+        if label_cut:
+            cuts_labels_ls = list(np.repeat(cuts, n))
+        if len(cuts)>1:
+            n = n*len(cuts)
+            name_ls = name_ls*len(cuts)
     k = 0
     if ncol is None:
         ncol = np.ceil(n / nrow).astype(int)
+    # to do: here assumes background means 3D, update 
     if background is None:
         cmap1 = colors.ListedColormap(['black', 'blue'])
     else:
         cmap1 = colors.ListedColormap(['none', 'blue'])
-        # get the brain image into the right direction
-        confset_ls = [[np.flipud(np.transpose(data[:, :, cut])) for data in list(confset) + [get_data(background)]] for
-                      confset in confset_ls]
+        confset_2d_ls = []
+        for cut in cuts:
+            confset_2d = return_cuts(confset_ls, display_mode=display_mode, cut=cut, background=background)
+            #print(len(confset_2d))
+            confset_2d_ls = confset_2d_ls + confset_2d
+        confset_ls = confset_2d_ls
+        #print(len(confset_ls))
         if label_cut:
-            name_ls = [name + f",cut at z={cut}" for name in name_ls]
+            name_ls = [name_cut[0]+f", {display_mode}={name_cut[1]}" for name_cut in zip(name_ls, cuts_labels_ls)]
     cmap2 = colors.ListedColormap(['none', 'yellow'])
     cmap3 = colors.ListedColormap(['none', 'red'])
 
@@ -91,67 +122,3 @@ def confset_plot(confset_ls, name_ls, nrow=1, ncol=None, fontsize=20,
 
     plt.suptitle(title)
     # plt.show()
-
-
-def ls_plot(image_ls, name_ls=None, nrow=1, ncol=None, fontsize=20, figsize=(30, 20),
-            title=None, titlesize=20,
-            colorbar=None, ticks=True):
-    """
-    plot a list of 2D images
-
-    Parameters
-    ----------
-    image_ls: list
-        a list of 2D arrays
-    name_ls: list
-        a list of names for each subplot
-    nrow: int, optional, default: 1
-        number of rows of the figure
-    ncol: int, optional, default: the length of signal_ls/nrow
-    fontsize : int, optional, default: 20
-        font size for figure
-    figsize : tuple, optional, default: (30,20)
-        figure size
-
-    Examples
-    --------
-    ls_plot(signal_ls, signal_name_ls)
-
-    """
-    n = len(image_ls)
-    # print(n)
-    k = 0
-    if ncol is None:
-        ncol = np.ceil(n / nrow).astype(int)
-        # print(ncol)
-    # a plot with multiple subplots
-    fig, axs = plt.subplots(nrow, ncol, figsize=figsize)
-    # print(axs.shape)
-    if nrow == 1:
-        axs = np.array([axs]).reshape((1, -1))
-    elif ncol == 1:
-        axs = np.array([axs]).reshape((-1, 1))
-    # each subplot
-    for i in range(nrow):
-        for j in range(ncol):
-            if k == n:
-                break
-            im = axs[i, j].imshow(image_ls[k])
-            if not ticks:
-                axs[i, j].get_xaxis().set_visible(False)
-                axs[i, j].get_yaxis().set_visible(False)
-
-            # title of the subplot
-            if name_ls is not None:
-                axs[i, j].set_title(name_ls[k], fontsize=fontsize)
-            k = k + 1
-            if colorbar == 'individual':
-                plt.colorbar(im)
-    if colorbar == 'share':
-        # [left, bottom, width, height] of the new axes
-        cbar_ax = fig.add_axes([0.95, 0.2, 0.015, 0.5])
-        fig.colorbar(im, cax=cbar_ax)
-    plt.suptitle(title, size=titlesize)
-
-    # plt.show()
-
